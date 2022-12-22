4b4946695d94fd518e1d2098177b4821f79b1063
Lucas Pina <ti.lucaspina@gmail.com>
2022-12-21 21:38:20 -0300

    implementando troca de temas

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import Header from '../Header';
-import PostsList from '../PostsList';
-import Footer from '../Footer';
+import Header from "../Header";
+import PostsList from "../PostsList";
+import Footer from "../Footer";
 
-export default function Layout() {
+export default function Layout({ onToggletheme, selectedTheme }) {
   return (
     <>
-      <Header />
+      <Header onToggletheme={onToggletheme} selectedTheme={selectedTheme} />
       <PostsList />
-      <Footer />
+      <Footer onToggletheme={onToggletheme} selectedTheme={selectedTheme} />
     </>
   );
 }
