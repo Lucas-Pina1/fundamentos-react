7aaf3aea9e2670300f9a63c0bfd906968e566d10
Lucas <lucas.pina@prodepa.pa.gov.br>
2022-12-22 11:49:43 -0300

    entendendo um pouco de useEffect e unMounted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyle from "./styles/global";
@@ -16,6 +16,10 @@ function App() {
   function handleToggleTheme() {
     setTheme((prevState) => (prevState === "dark" ? "light" : "dark"));
   }
+
+  // useEffect(() => {
+  //   localStorage.setItem('theme', JSON.stringify(theme));
+  // }, [theme]);
 
   return (
     <ThemeProvider theme={currentTheme}>
